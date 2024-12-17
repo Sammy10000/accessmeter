6387c1db81fa7e86f2e350007a401ea798576357
Samuel Enyi <admin@accessmeter.com>
2024-12-17 23:21:18 +0100

    Let's begin creating the next valuable plugin for wordpress site accessibility

---
+++ accessmeter.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Accessmeter
+Plugin URI: https://github.com/Sammy10000/accessmeter
+Description: Real-time automated accessibility reports using AXE and Lighthouse APIs, and full issues remediation support.
+Version: 1.0
+Author: Samuel Enyi
+Author URI: https://accessmeter.com
+License: GPL2
+*/
+
+
